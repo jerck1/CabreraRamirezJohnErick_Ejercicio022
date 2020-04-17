93087441d16f8694739a6747cc2dcdb95c4610a7
jerck1 <johncafre@gmail.com>
2020-04-17 12:05:36 -0500

    pushing

--- mlp_digitos.py
+++ mlp_digitos.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import numpy as np
-# import matplotlib.pyplot as plt
-# import sklearn.datasets
-# import sklearn.preprocessing
-# import sklearn.neural_network
-# import sklearn.model_selection
+import matplotlib.pyplot as plt
+import sklearn.datasets
+import sklearn.preprocessing
+import sklearn.neural_network
+import sklearn.model_selection
 
 
 # In[2]:
@@ -37,7 +37,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 
-# In[6]:
+# In[21]:
 
 
 #Number of neurons
@@ -45,25 +45,25 @@ n=np.arange(1,21)
 #loss
 loss=[]
 #f1_score
-f1_s=[]
+f1_s_train=[]
+f1_s_test=[]
 
 
-# In[5]:
+# In[22]:
 
 
 for i in n:
     mlp = sklearn.neural_network.MLPClassifier(activation='logistic', 
                                            hidden_layer_sizes=(i), 
                                            max_iter=200)
-    #mlp.fit(X_train, Y_train)
-#     if(i==5):
-#         opt_coef=mlp.coefs_
+    mlp.fit(X_train, Y_train)
     loss=np.append(loss,mlp.loss_)
-    f1_s=np.append(f1_s,sklearn.metrics.f1_score(Y_test, mlp.predict(X_test), average='macro'))
+    f1_s_train=np.append(f1_s_train,sklearn.metrics.f1_score(Y_train, mlp.predict(X_train), average='macro'))
+    f1_s_test=np.append(f1_s_test,sklearn.metrics.f1_score(Y_test, mlp.predict(X_test), average='macro'))
     #print('Loss', mlp.loss_)#entropía relativa, divergencia
 
 
-# In[ ]:
+# In[23]:
 
 
 plt.figure(1,figsize = (10,10))
@@ -72,37 +72,51 @@ plt.plot(n,loss)
 plt.xlabel('n neurons')
 plt.ylabel('loss')
 plt.subplot(1,2,2)
-plt.plot(n,loss)
+plt.plot(n,f1_s_train,label="train")
+plt.plot(n,f1_s_test,label="test")
 plt.xlabel('n neurons')
 plt.ylabel('f_1 score')
 plt.savefig("loss_f1.png")
 
 
-# In[ ]:
+# In[24]:
 
 
-n_opt=5
+#coeficientes para el número de neuronas óptimo
+n_opt=7
 mlp = sklearn.neural_network.MLPClassifier(activation='logistic', 
                                            hidden_layer_sizes=(n_opt), 
                                            max_iter=200)
-#mlp.fit(X_train, Y_train)
+mlp.fit(X_train, Y_train)
 opt_coef=mlp.coefs_
 loss=np.append(loss,mlp.loss_)
 f1_s=np.append(f1_s,sklearn.metrics.f1_score(Y_test, mlp.predict(X_test), average='macro'))
 
 
-# In[ ]:
+# In[28]:
 
 
-plt.figure(figsize=(10, 5))
+plt.figure(figsize=(30, 20))
 scale = np.max(mlp.coefs_[0])
 plt.figure()
-for i in range(5):
-    l1_plot = plt.subplot(2, 5, i + 1)
+for i in range(7):
+    l1_plot = plt.subplot(4, 2, i + 1)
     l1_plot.imshow(opt_coef[0][:,i].reshape(8, 8), interpolation='nearest',
                    cmap=plt.cm.RdBu, vmin=-scale, vmax=scale)
     l1_plot.set_xticks(())
     l1_plot.set_yticks(())
     l1_plot.set_xlabel('Neuron %i' % i)
 plt.savefig("neuronas.png")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
